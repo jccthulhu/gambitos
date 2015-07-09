664c1f05d7a1e5b9b6becab06d26ed8cd0570d1d
John Church <jccthulhu@gmail.com>
2015-07-08 22:03:30 -0700

    Fixing certain bugs and verifiying certain things

--- boot/boot1.s
+++ boot/boot1.s
@@ -259,11 +259,6 @@ loadprt:
 	pushw	%dx
 	pushw	%bx
 	movw	%bx,%si
-	# DEBUG
-	pushw	%ax
-	callw	putint
-	callw	putn
-	popw	%ax
 	# calculate index
 	movw	$PART_SZ,%di
 	pushw	%dx
@@ -272,11 +267,6 @@ loadprt:
 	movw	%ax,%di
 	# get the pointer to the partition table
 	addw	$PART_TBL,%di
-	# DEBUG
-	pushw	%ax
-	movw	%di,%ax
-	callw	putint
-	popw	%ax
 	# step to its LBA descriptor
 	addw	$0x08,%di
 	# load the number of sectors
@@ -284,11 +274,6 @@ loadprt:
 	# load the index of the first sector
 	# there's no way we can use the upper 4 bytes
 	movw	0x6(%di),%di
-	# DEBUG
-	pushw	%ax
-	movw	%di,%ax
-	callw	getint	
-	popw	%ax
 loadprt.0:
 	# get the drive geometry
 	callw	getgeo		# bx <- number_of_heads:sectors_per_head

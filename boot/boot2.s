d671f62810c171973d8686ac04a16c0c8500c3b4
John Church <jccthulhu@gmail.com>
2015-07-10 21:10:28 -0700

    Adding second stage of boot sequence

---
+++ boot/boot2.s
@@ -0,0 +1,31 @@
+	.globl	start
+	.code16
+
+	.set STACK_TOP,0x7000
+
+start:
+	# clear segments
+	xorw	%ax,%ax
+	movw	%ax,%es
+	movw	%ax,%ds
+	movw	%ax,%ss
+	# set up the stack
+	movw	$STACK_TOP,%sp
+	# DEBUG
+	movb	$0x42,%al
+	callw	putchr
+	jmp	.
+
+# prints character in %al
+putchr:
+	# save registers
+	pushw	%bx
+	pushw	%ax
+	movw	$0x7,%bx
+	movb	$0xe,%ah
+	int	$0x10
+	# TODO: error handling
+	# restore registers
+	popw	%ax
+	popw	%bx
+	retw

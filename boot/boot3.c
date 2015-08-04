efcc256306047f1487241f352379f543447e3754
John Church <jccthulhu@gmail.com>
2015-08-04 14:55:05 -0700

    Adding C entry point and updating Makefile

---
+++ boot/boot3.c
@@ -0,0 +1,49 @@
+/**
+ *	boot3.c
+ *
+ *	"Third" stage bootloader; gets loaded with the second stage, but is the first C code we can use in the boot process
+ **/
+
+// definitions
+
+#define	VIDEO_MEM	0xb8000
+#define	VIDEO_MEM_SIZE	0xfa0
+
+void putchr( char );
+
+// entry point
+
+void start()
+{
+	// variables
+	// function body
+	putchr( '!' );
+
+	// clean up
+	// next point
+}
+
+// data members
+
+char * currentVideo = VIDEO_MEM;
+
+// routine definitions
+
+void putchr( char c )
+{
+	// variables
+
+	// function body
+	// write the character
+	currentVideo[0] = c;
+	// write the attributes
+	currentVideo[1] = 0x07;
+	// increment
+	currentVideo += 2;
+	// maybe wrap
+	if ( ((int)( currentVideo - VIDEO_MEM )) > VIDEO_MEM_SIZE ) { currentVideo = (char*)VIDEO_MEM; }
+
+	// clean up
+	return;
+}
+

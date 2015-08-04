8f1f63e5e5cf12fe2537e82edfe530bcfe963783
John Church <jccthulhu@gmail.com>
2015-08-04 15:21:24 -0700

    Stripped out 32 bit main

--- boot/boot2.s
+++ boot/boot2.s
@@ -49,170 +49,29 @@ start:
 	# enable more than 1MB of memory, more than anyone would ever need
 	callw	seta20		# call the 'seta20' subroutine to the A20 line, which will stop the automatic wrapping of memory addresses past 1MB
 	
-	lgdt	gdtdesc		# load GDT by passing in the address of the GDT descriptor
-				# GNU requires that this instruction take a label instead of a regsiter value
-
-	# get us into protected mode so that we can then jump into 32 bit mode
-	# protected mode is necessary for virtual memory
-	mov	%cr0,%eax	# set %eax to %cr0, the 0th control register
-				# %cr9 is a bit field that holds information for protected mode, paging enabled, floating point emulation, and more
-	orb	$0x1,%al	# set the protected mode bit in the copied bit field to 1
-	mov	%eax,%cr0	# write the modified bit field to %cr0, the 0th control register
-	# set up the Task State Segment (TSS)
-	movw	$TSS_SPC,%di	# set %di to the TSS pointer
-	callw	createtss	# call the 'createtss' subroutine to setup the TSS
-	ljmp	$CODE_SEL,$main	# jump to the 32 bit mode entry point (main) and set the code segment register to the value in the proper GDT index
-
-###
-# sets the A20 line, the 21st bit of memory addresses, to enable more than 1MB of memory
-# params: none
-# returns: none
-# note: the A20 line is effectively, in the past, off the CPU within the keyboard, so we have to communicate as though it's off the chip
-seta20:
-	pushw	%ax		# save %ax to the stack
-seta20.1:
-	inb	$0x64,%al	# read the keyboard control chip's status register to see if it's busy
-	testb	$0x2,%al	# is the keyboard control chip busy?
-	jnz	seta20.1	# if the keyboard control chip is busy, loop and try again (busy wait)
-
-	# if the keyboard control chip is ready for commands
-	movb	$0xd1,%al	# set %al to the value of the chip's write command, which is 0xd1
-	outb	%al,$0x64	# send the command within %al to the keyboard control chip
-seta20.2:
-	inb	$0x64,%al	# read the keyboard control chip's status register to see if it's busy
-	testb	$0x2,%al	# is the keyboard control chip busy?
-	jnz	seta20.2	# if the keyboard control chip is busy, loop and try again (busy wait)
-
-	# if the keyboard control chip is ready for commands
-	movb	$0xdf,%al	# set %al to the value of the chip's enable command, which is 0xdf
-	outb	%al,$0x60	# write out the command within %al to the keyboard control chip to finally enable the A20 line
-
-	popw	%ax		# reset %ax to its value from the stack
-	retw			# return from the subroutine
-
-
-
-###
-# print a character to the screen
-# note: this is for use in real mode, and will cause a triple fault in protected mode
-# params:
-#	al	the character (ASCII value)
-putchr:
-	# save register values to the stack
-	pushw	%bx
-	pushw	%ax
-
-	movw	$0x7,%bx	# print white characters on black background
-	movb	$0xe,%ah	# when we're stil in real mode, set %al to 0xe, which is the bios call to print to the screen
-	int	$0x10		# trigger the interrupt
-	# restore register values from the stack
-	popw	%ax
-	popw	%bx
-	retw			# return from the subroutine
-
-
-
-###
-# this is the global descriptor table, which describes memory segments;
-# we currently use the bare minimum table because this table is quickly
-# abandoned in our jump to 64 bit
-# note: two words is four bytes
-gdt:
-	# null entry
-	.word	0x0	# zero limit
-	.word	0x0	# zero base
-	.byte	0x0	# zero base some more
-	.byte	0x10	# access byte
-	.byte	0xc0	# flags:limit
-	.byte	0x0	# zero base
-	# kernel code segment
-	.word	0xffff	# high limit	
-	.word	0x0	# zero base
-	.byte	0x0	# zero base some more
-	.byte	0x9a	# access byte
-	.byte	0xcf	# flags:high limit
-	.byte	0x0	# zero base some more
-	# kernel data segment
-	.word	0xffff	# high limit
-	.word	0x0	# zero base
-	.byte	0x0	# zero base some more
-	.byte	0x92	# access byte
-	.byte	0xcf	# flags:high limit
-	.byte	0x0	# revenge of the zero base
-	# TSS segment
-	.word	TSS_SZ+TSS_SPC	# 100 byte limit
-	.word	TSS_SPC	# base
-	.byte	0x0	# zero the rest of the base
-	.byte	0x89	# access byte
-	.byte	0x40	# flags:low limit
-	.byte	0x0	# zero the rest of the base
-
-gdtdesc:
-	.word	GDT_SIZE-1
-	.word	gdt		# endianness swap
-	.word	0x0
-
-
-
-###
-# create the TSS in memory
-# params:
-#	di	a pointer to the TSS
-createtss:
-	movw	$DATA_SEL,0x8(%di)	# write data segment index into the stack segment slot of the TSS
-	movw	$STACK_TOP,0x4(%di)	# write the stack origin pointer into the kernel stack slot of the TSS
-	movw	$TSS_SZ,0x66(%di)	# write the size of the TSS into the io bitmap pointer slot of the TSS
-					# we do this because we don't use the io bitmap, so any data pointer will do
-	retw
-
-
-
-
-	.code32		# start the 32 bit protected mode code
-
-# 32 bit main
-main:
-	# set up the 32 bit stack
-	xor	%ecx,%ecx
-
-	movb	$DATA_SEL,%cl		# set %cl to the data segment index
-	movw	%cx,%ss			# set %ss, the stack segment, to %cx, the data segment index
-
-	# set up the TSS
-	movw	$TSS_SEL,%ax		# set %ax to the TSS index
-	ltr	%ax			# this is the 'load TSS register' command, which loads the TSS from %ax
-
-	# DEBUG
-	call	prclrscrn		# call the subroutine that clears the screen
-	movl	$port_msg,%eax		# load the 32 bit protected mode welcome message
-	call	prputstr		# print the message to the screen
-	call	prputn			# print a new line
-	# END DEBUG
-
-	# now we will try to jump into 64 bit mode
+	lgdt	gdt64desc	# load the long mode GDT by passing in the address of the long mode GDT descriptor
+				# GNU requires that this instruction take a label instead of a register value
 
 	# check for CPUID extended
 	movl	$0x80000000,%eax	# CPUID parameter; asks for the highest value of that parameter supported
 	cpuid				# get CPU information specified in %eax
 	movl	$0x80000001,%ebx	# see if the CPUID operation can take the extended bit value
 	cmpl	%eax,%ebx		# compare %eax, the result of CPUID, to %ebx, the minimum supported value we need
-	jg	main.0			# if the CPUID operation does not support extended features, jump to an error state
+	jg	start.0			# if the CPUID operation does not support extended features, jump to an error state
 
 	# check for 64 bit support
 	movl	$0x80000001,%eax	# CPUID parameter; asks for list of supported features
 	cpuid				# get CPU information specified in %eax
 	andl	$0x20000000,%edx	# mask out the long mode bit from the result of the CPUID operation
 	testl	%edx,%edx		# is the long mode bit set in the result from the CPUID operation?
-	je	main.1			# if the long mode bit is clear (zero), jump to the error state
+	je	start.1			# if the long mode bit is clear (zero), jump to the error state
 
 	# if the CPUID extended bit is set, adn there is 64 bit support, we start going into 64 bit mode
 
-	# DEBUG
-	movl	$do_long_mode_msg,%eax	# load a message notifying the user of our intent to boot into
-					# long mode
-	call	prputstr		# print the message
-	call	prputn			# print a new line
-	# END DEBUG
+	# set protected mode
+	mov	%cr0,%eax	# load control register zero
+	orb	$0x1,%al	# set the PM bit
+	mov	%eax,%cr0	# write the control register
 
 	call	disable_paging		# call the disable paging subroutine so that we can construct the page table
 	call	create_page_tables	# call the subroutine to create the page tables
@@ -220,37 +79,13 @@ main:
 	call	enable_lm		# call the subroutine to finally enable long mode
 	call	enable_paging		# call the subroutine to enable paging
 
-	# DEBUG
-	movl	$long_mode_comp_msg,%eax	# load a message telling the user that we are now in 
-					# IA-32e long mode compatibility mode
-	call	prputstr		# print the message
-	call	prputn			# print a new line
-	# END DEBUG
+	ljmp	$CODE_SEL,$main64	# jump to the 32 bit entry point
 
-	lgdt	gdt64desc		# load the long mode descriptor table;
-					# see, I told you we were just gonna ditch that other one
-	ljmp	$0x08,$main64		# jump to the 64 bit entry point and set the code segment register to the value in the proper GDT index
-
-# print an error message that the CPU doesn't support extended features
-main.0:
-	movl	$no_cpuid_msg,%eax	# load a message telling the user that their CPU sucks a lot
-	call	prputstr		# print it
-	call	prputn			# print a new line
-	jmp	main.2			# jump to error handling
-
-# print an error message that the CPU doesn't support 64 bit mode
-main.1:
-	movl	$no_long_mode_msg,%eax	# load a message telling the user that their CPU sucks a little
-	call	prputstr		# print it
-	call	prputn			# print a new line
-	jmp	main.2			# jump to error handling
-
-main.2:
-	# TODO: Either load a 32 bit version of the kernel or Michael Bay-splode
-	# because we don't support 32 bit CPUs
-	jmp	.			# create a tight loop to halt the program
-
-
+start.0:
+	# TODO: Warning, no support for CPUID extended
+start.1:
+	# TODO: Warning, no support for long mode
+	jmp	.
 
 
 ###
@@ -267,17 +102,8 @@ disable_paging:
 	movl	%cr0,%eax		# load the 0th control register into %eax
 	andl	$0x7FFFFFFF,%eax	# clear the paging bit from %eax
 	movl	%eax,%cr0		# write %eax back into the 0th control register
-
-	# DEBUGGING
-	movl	$pgng_off_msg,%eax	# notify the user that paging is off
-	call	prputstr		# print that message
-	call	prputn			# print a new line
-	# END DEBUGGING
-
 	popl	%eax			# restore %eax from the stack
 	ret				# return from the subroutine
-
-
 
 ###
 # enable paging
@@ -288,17 +114,8 @@ enable_paging:
 	movl	%cr0,%eax		# load the 0th control register into %eax
 	orl	$0x80000000,%eax	# set the paging bit in %eax
 	movl	%eax,%cr0		# write %eax back into the 0th control register
-
-	# DEBUGGING
-	movl	$pgng_on_msg,%eax	# notify the user that paging is on
-	call	prputstr		# print that message
-	call	prputn			# print a new line
-	# END DEBUGGING
-
 	popl	%eax			# restore the value of %eax from the stack
 	ret				# return from the subroutine
-
-
 
 ###
 # enable physical address extension (PAE)
@@ -311,8 +128,6 @@ enable_pae:
 	movl	%eax,%cr4		# write %eax into the 4th control register
 	popl	%eax			# restore value of register from stack
 	ret				# return from the subroutine
-
-
 
 ###
 # enable long mode
@@ -402,154 +217,42 @@ create_page_tables.0:
 	popl	%eax
 	ret				# return from thie subroutine
 
+# sets the A20 line to enable >1MB of memory
+# accepts no parameters, returns no values
+seta20:
+	pushw	%ax
+seta20.1:
+	inb	$0x64,%al	# get status
+	testb	$0x2,%al	# busy?
+	jnz	seta20.1	# yes
+	movb	$0xd1,%al	# commands: write
+	outb	%al,$0x64	#	output port
+seta20.2:
+	inb	$0x64,%al	# get status
+	testb	$0x2,%al	# busy?
+	jnz	seta20.2	# yes
+	movb	$0xdf,%al	# enable
+	outb	%al,$0x60	# 	A20
+	popw	%ax
+	retw
 
-
-###
-# data used for printing stuff
-current_video_mem:
-	.long	VIDEO_BASE
-	.byte	0x0		# x
-	.byte	0x0		# y
-
-
-
-###
-# clear the screen
-# params: none
-# returns: none
-prclrscrn:
-	# save register values to the stack
-	pushl	%eax
-	pushl	%ecx
-
-	movl	$current_video_mem,%eax	# load the pointer to video memory into %eax
-	movl	$VIDEO_BASE,(%eax)	# write our video memory structure data into video memory
-	movw	$0x0,0x4(%eax)		# zero out the fourth and fifth indices within the video memory structure
-					# these two indices hold the x and y poitition of the cursor respectively
-	movl	$0x25,%ecx		# use %ecx as a counter for the loop command
-					# TODO: we're not sure if 0x25 is the correct amount of lines on screen...
-# a loop to clear each line of the screen
-prclrscrn.0:
-	call	prputn			# call the prputn subroutine print a clear line onto the screen
-	loop	prclrscrn.0		# while %ecx is non-zero, decrement its value and continue through the loop
-
-	# reset the video memeory structure
-	movl	$current_video_mem,%eax	# load the pointer to the video memory structure into %eax
-	movl	$VIDEO_BASE,(%eax)	# write our video memory structure data into video memory
-	movw	$0x0,0x4(%eax)		# zero out the fourth and fifth indices within the video memory structure
-					# resets the x and y position of the cursor
-
-	# restore register values to the stack
-	popl	%ecx
-	popl	%eax
-	ret				# return from the subroutine
-
-
-
-###
-# print a string onto the screen
+# print a character to the screen
 # params:
-# 	eax	a poiter to the string to print to the screen
-# returns: none
-prputstr:
+#	al	the character (ASCII value)
+putchr:
 	# save register values to the stack
-	pushl	%eax		
-	pushl	%edi
-
-	movl	%eax,%edi		# save the string pointer into %edi
-	xorl	%eax,%eax		# zero out %eax by XORing itself
-prputstr.0:
-	movb	(%edi),%al		# load the character pointed to by the string pointer into %al
-	testb	%al,%al			# test to see if the character in %al is zero
-	je	prputstr.1		# if the character is zero, we're done with the line and jump to the end of the subroutine
-
-	# if the character in %al is non-zero
-	call	prputchr		# call the prputchr subroutine to print the character onto the screen
-	incl	%edi			# increment the string pointer in %edi so we can access the next character within the string
-	jmp	prputstr.0		# jump to the start of the loop to continue printing the string
-
-prputstr.1:
+	pushw	%bx
+	pushw	%ax
+	movw	$0x7,%bx	# white characters on black background
+	movb	$0xe,%ah	# bios call E
+	int	$0x10		# trigger the interrupt
 	# restore register values from the stack
-	popl	%edi
-	popl	%eax
-	ret				# return from the subroutine
+	popw	%ax
+	popw	%bx
+	retw
 
-
-
-###
-# print a new line onto the screen
-# params: none
-# returns: none
-prputn:
-	# save registers onto the stack
-	pushl	%eax
-prputn.0:
-	# print space
-	movl	$0x20,%eax		# load 0x20, which is the ASCII value for the space character, into %eax
-	call	prputchr		# call the prputchr subroutine to print the space character onto the screen
-
-	# update the position of the cursor in the video memory structure
-	movl	$current_video_mem,%eax	# load the pointer to the video memory structure into %eax
-	movb	0x4(%eax),%al		# load the x position of the cursor, which is in the fourth index of the structure, into %al
-	testb	%al,%al			# test to see if the value of %al is zero
-	jz	prputn.1		# if the value of %al is zero, jump to the end of the subroutine
-	
-	# if the value of %al is non-zero
-	jmp	prputn.0		# jump to the beginning of the loop to print into the next character location
-prputn.1:
-	# restore the registers
-	popl	%eax
-	ret				# return from the subroutine
-
-
-
-###
-# prints a character onto the screen
-# params:
-# 	eax	the ASCII character value to be printed onto the screen
-# returns: none
-prputchr:
-	# save the registers
-	pushl	%edi
-	pushl	%esi
-	pushl	%eax
-
-	# load the character into video memory
-	movl	$current_video_mem,%edi	# load the pointer to the video memory struct into %edi
-	movl	(%edi),%esi		# load the zeroth index of the video memory structure into %esi
-					# this value is a pointer to the next space in video memory to print to
-	movb	%al,(%esi)		# write %al, the character value, into the current space in video memory
-	movb	$TEXT_ATTR,0x1(%esi)	# write the appropriate attribute bits into the character's attributes section within video memory
-
-	# increment the video memory pointer
-	addl	$0x2,%esi		# increment %esi by two, which will result in %esi pointing to the next location in video memory
-	movl	%esi,(%edi)		# write the current pointer to video memory into the video memory structure's zeroth index
-
-	xorl	%eax,%eax		# zero out %eax by XORing itself
-	movb	0x4(%edi),%al		# write the current x position of the cursor into %al
-	incl	%eax			# increment the position of the cursos's x position within %eax
-	movl	$0x50,%esi		# set %esi to be 81, which is the cursor position just after the end of the screen
-	cmpl	%eax,%esi		# compare %eax to %esi to see if the cursor's next x position will be off the screen
-	jne	prputchr.0		# if the cursor will be moved to a valid location on the line, jump to prputchr.0 to finish the subroutine
-
-	# if the cursor would be moved off the edge of the screen
-	xorl	%eax,%eax		# set %eax to zero by XORing itself
-	movb	%al,0x4(%edi)		# write the new x location of the cursor into the video memory structure
-	movb	0x5(%edi),%al		# load the y location of the cursor from the video memory structure into %al
-	incl	%eax			# increment the y location of the cursor within %eax
-	movb	%al,0x5(%edi)		# write the new y location of the cursor into the video memory structure
-	jmp	prputchr.1		# jump to the end of the subroutine to return
-
-# if the cursor will remain on the same line
-prputchr.0:
-	movb	%al,0x4(%edi)		# write the new x location of the cursor into the video memory structure
-prputchr.1:
-	# restore registers
-	popl	%eax
-	popl	%esi
-	popl	%edi
-	ret				# return from the subroutine
-
+	.code32
+>>>>>>> Stripped out 32 bit main
 
 
 ###
